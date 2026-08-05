9f3f4d049b0b1d087fab7dbb80c1892acdbba915
caritemanbot <ababilwirawijaya2@gmail.com>
2026-08-06 02:07:59 +0700

    Update index.js

--- modules/news/index.js
+++ modules/news/index.js
@@ -1,7 +1,19 @@
-async function checkTONNews() {
-    console.log("Checking TON news...");
+const { saveNews } = require("../../services/database/news");
+
+async function processNews(items){
+
+    for(const item of items){
+
+        const isNew = await saveNews(item);
+
+        if(isNew){
+            console.log("NEW:",item.title);
+        }
+
+    }
+
 }
 
-module.exports = {
-    checkTONNews
-};
+module.exports={
+    processNews
+}
