f77f129e18414354c9fe350fa35bea899132c00f
caritemanbot <ababilwirawijaya2@gmail.com>
2026-08-06 02:31:53 +0700

    Create demo.js

---
+++ modules/projects/sources/demo.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "Demo Project Source",
+
+  async fetch() {
+    return [
+      {
+        name: "TON Starter",
+        website: "https://example.com",
+        description: "New TON ecosystem project",
+        source: "Demo"
+      }
+    ];
+  }
+};
