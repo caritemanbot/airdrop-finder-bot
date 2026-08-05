50a2b094ecc8877949bbc80047be45a0ec9fae81
caritemanbot <ababilwirawijaya2@gmail.com>
2026-08-06 02:09:16 +0700

    Update index.js

--- modules/news/index.js
+++ modules/news/index.js
@@ -1,14 +1,29 @@
-const { saveNews } = require("../../services/database/news");
+const bot=require("../../services/telegram");
+
+const {GROUP_ID}=require("../../config/config");
+
+const {saveNews}=require("../../services/database/news");
 
 async function processNews(items){
 
     for(const item of items){
 
-        const isNew = await saveNews(item);
+        const isNew=await saveNews(item);
 
-        if(isNew){
-            console.log("NEW:",item.title);
-        }
+        if(!isNew) continue;
+
+        await bot.sendMessage(
+            GROUP_ID,
+
+`📰 TON News
+
+${item.title}
+
+Source:
+${item.source}
+
+${item.url}`
+        );
 
     }
 
