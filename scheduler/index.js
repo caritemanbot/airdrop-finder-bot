2a96c90b12e9f52e69801c3f52efb5a4b93596c9
caritemanbot <ababilwirawijaya2@gmail.com>
2026-08-06 02:05:27 +0700

    Update index.js

--- scheduler/index.js
+++ scheduler/index.js
@@ -1,7 +1,18 @@
 const cron = require("node-cron");
 
-cron.schedule("*/10 * * * *", () => {
-  console.log("⏰ Scheduler is running...");
+const sources = require("../modules/sources");
+const demo = require("../modules/news/sources/demo");
+
+sources.register(demo);
+
+cron.schedule("*/10 * * * *", async () => {
+
+    console.log("Checking sources...");
+
+    const news = await sources.fetchAll();
+
+    console.log(news);
+
 });
 
-console.log("✅ Scheduler started.");
+console.log("Scheduler started.");
