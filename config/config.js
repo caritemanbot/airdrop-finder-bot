13bed317e69a63abb6132d1aa850fc0b52b2cfca
caritemanbot <ababilwirawijaya2@gmail.com>
2026-08-06 02:18:01 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,5 +3,6 @@ require("dotenv").config();
 module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN,
   GROUP_ID: process.env.GROUP_ID,
-  DATABASE_URL: process.env.DATABASE_URL
+  DATABASE_URL: process.env.DATABASE_URL,
+  NEWSDATA_API_KEY: process.env.NEWSDATA_API_KEY
 };
