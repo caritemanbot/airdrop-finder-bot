4b8f37579eec1253b7b449e867ded5c3d82ea866
caritemanbot <ababilwirawijaya2@gmail.com>
2026-08-05 21:33:51 +0700

    Create telegram.js

---
+++ services/telegram.js
@@ -0,0 +1,8 @@
+const TelegramBot = require("node-telegram-bot-api");
+const { BOT_TOKEN } = require("../config/config");
+
+const bot = new TelegramBot(BOT_TOKEN, {
+  polling: true
+});
+
+module.exports = bot;
