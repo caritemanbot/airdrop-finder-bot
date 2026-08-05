41e9d14ac0a1846b7af533261ce9fe5dcf74d66e
caritemanbot <ababilwirawijaya2@gmail.com>
2026-08-06 03:06:36 +0700

    Delete config directory

--- config/config.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  BOT_TOKEN: process.env.BOT_TOKEN,
-  GROUP_ID: process.env.GROUP_ID,
-  DATABASE_URL: process.env.DATABASE_URL,
-  NEWSDATA_API_KEY: process.env.NEWSDATA_API_KEY
-};
